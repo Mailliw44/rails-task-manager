7ed296f7bed06620133ae45d2ff89a27cb005282
ZEROUKI <zerouki@mac.home>
2025-08-12 17:00:52 +0200

    ex

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # # Je peux voir toutes les taches READ
+  get "tasks", to: "tasks#index"
+
+  # # J'affiche le formulaire de création d'un task
+  get "tasks/new", to: "tasks#new"
+
+  # # Je crée une nouvelle instance de tache CREATE
+  post "tasks", to: "tasks#create"
+
+  # # J'affiche le formulaire de modification d'une tache
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  # # Je modifie 1 task UPDATE
+  patch "tasks/:id", to: "tasks#update"
+
+  # # Je supprime un task DELETE
+  delete "tasks/:id", to: "tasks#destroy"
+
+  # # J'accède au détail d'1 task
+  get "tasks/:id", to: "tasks#show", as: "task"
 end
+
